571488ea215c764dbcc5222aba5267434f15f268
byMaxKucher <63538024+MaxDevIos@users.noreply.github.com>
2021-07-23 12:36:48 +0300

    Initial Commit

---
+++ exploredSegueBetweenView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  exploredSegueBetweenView
+//
+//  Created by Maxim Kucherov on 23.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
